ed4d06fba9d3aa652d6a48d6984da0fdfa1b18e1
Luis Salas <lhozdroid@gmail.com>
2024-06-04 17:26:29 -0400

    Removed terser because it was causing issues.

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,16 +1,10 @@
-import terser from "@rollup/plugin-terser";
-
 export default {
     input: "src/bmodal.js",
     output: [
         {
-            file: "dist/bmodal.esm.js",
+            file: "dist/bmodal.es.js",
+			name: "bmodal.es.js",
             format: "es"
-        },
-        {
-            file: "dist/bmodal.cjs.js",
-            format: "cjs"
         }
-    ],
-    plugins: [terser()]
+    ]
 };
